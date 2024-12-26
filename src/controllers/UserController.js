7f4e89cacd5ee6c984981c85daa09c7c6de9b7a3
Aydom Aparecido dos Santos <101830296+AydomSantos@users.noreply.github.com>
2024-12-26 20:28:37 -0300

    correções

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,39 +13,42 @@ class UserController {
     }
   }
 
-  // Index
   async index(req, res) {
     try {
       const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
-      return res.json(null);
+      return res.status(400).json({
+        errors: ['Erro ao listar usuários'],
+      });
     }
   }
 
-  // Show
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não encontrado'],
+        });
+      }
       const { id, nome, email } = user;
       return res.json({ id, nome, email });
     } catch (e) {
-      return res.json(null);
+      return res.status(400).json({
+        errors: ['Erro ao buscar usuário'],
+      });
     }
   }
 
-  // Update
   async update(req, res) {
     try {
       const user = await User.findByPk(req.userId);
-
       if (!user) {
         return res.status(400).json({
           errors: ['Usuário não existe'],
         });
       }
-
       const novosDados = await user.update(req.body);
       const { id, nome, email } = novosDados;
       return res.json({ id, nome, email });
@@ -56,22 +59,21 @@ class UserController {
     }
   }
 
-  // Delete
   async delete(req, res) {
     try {
       const user = await User.findByPk(req.userId);
-
       if (!user) {
         return res.status(400).json({
           errors: ['Usuário não existe'],
         });
       }
-
       await user.destroy();
-      return res.json(null);
+      return res.json({
+        success: 'Usuário deletado com sucesso',
+      });
     } catch (e) {
       return res.status(400).json({
-        errors: e.errors.map((err) => err.message),
+        errors: ['Erro ao deletar usuário'],
       });
     }
   }
